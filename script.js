df3e24ae7e29403839c8a25519f3e4f2def8c28b
Gabriel Brasileiro <gabrasileiro_94@hotmail.com>
2023-04-05 11:40:57 -0300

    quase finalizado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const mario = document.querySelector('.pulo');
+const mario = document.querySelector('.mario');
+const cano = document.querySelector('.cano');
 
 const pulo = () => {
     mario.classList.add('pulo');
@@ -7,6 +8,27 @@ const pulo = () => {
         mario.classList.remove('pulo');
     }, 500)
 }
+
+const sePerdeu = setInterval(() => {
+
+    const posicaoCano = cano.offsetLeft;
+    const posicaoMario = +window.getComputedStyle(mario).bottom.replace(`px`, ``);
+
+    if (posicaoCano <= 120 && posicaoCano > 0 && posicaoMario < 81) {
+
+        cano.style.animation = 'none';
+        cano.style.left=`${posicaoCano}px`; 
+
+        mario.style.animation = 'none';
+        mario.style.bottom=`${posicaoMario}px`  
+
+        mario.src =`./images/mario_death.png`;
+        mario.style.width = `50px`;
+        mario.style.marginLeft = '50px'
+
+        clearInterval(sePerdeu)
+    }
+}, 10)
 
 
 document.addEventListener("keydown", pulo)
